090e2e129b3d36fbfdaad9007839cfa82f16afd2
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2020-09-02 22:32:20 -0700

    total months successful

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,10 +3,10 @@ import os
 import csv
 
 #store the file path associated with budget_data.csv across operating systems
-csvpath = os.path.join('..','Resources','budget_data.csv')
+csvpath = os.path.join('..','PyBank','Resources','budget_data.csv')
 
 #open the file using csv module
-with open(csvpath, newline="") as csvfile:
+with open(csvpath) as csvfile:
     #specify delimiter and variable "csvreader" that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     #store a reference to a file stream
@@ -16,10 +16,24 @@ with open(csvpath, newline="") as csvfile:
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
     
+    #Find total number of months included in the data
+    ##TOTAL MONTHS
+    total_months = len(list(csvreader))
+    print(f'Total Months: {total_months}')
+    
+    #The net total amount of "Profit/Losses" over the entire period
+    
+    
+    #The average of the changes in "Profit/Losses" over the entire period
+    
+    #The greatest increase in profits (date and amount) over the entire period
+    
+    
+    #The greatest decrease in losses (date and amount) over the entire period
+
+
     #read each row of data after the header
     for row in csvreader:
         print(row)
-        
-    
-    
-    
+          
+
