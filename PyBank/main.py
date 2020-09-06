e33171a358d4d5e864b03bc036b6e0f324fb5f24
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2020-09-06 14:24:29 -0700

    updated the for row with months inside loop

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,41 +6,39 @@ import csv
 csvpath = os.path.join('..','PyBank','Resources','budget_data.csv')
 col1 = []
 col2 = []
-total_months = []
-profit = []
-total_loss = (0)
-total_profit = (0)
+months = []
+total_months = 0
+amount = 0
+total_loss = 0
+total_profit = 0
 
 #open the file using csv module
 with open(csvpath, newline='') as csvfile:
     #specify delimiter and variable "csvreader" that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
     #store a reference to a file stream
-    print(csvreader)
+    #print(csvreader)
     #read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    
-    for rows in csvreader:
-        months = rows(0)
-        total_months = len(Months)
-    #create a variable that stores the profit column
-    amount_column = axis(0,:)
-    for cells in amount_column:`
-        total_profit.append
-        profit = int((rows[1]))
+    #print(f"CSV Header: {csv_header}")
+          
+    for row in csvreader:
+        #Find the length of the list of months
+        total_months = total_months + 1
         
-        if profit >= 0:
-            total_profit = total_profit + profit
-        elif profit < 0:
-            total_loss = total_loss + profit
-    
+        amount = int((row[1]))
+       
+        if amount >= 0:
+            total_profit = total_profit + amount
+           
+
+        if amount < 0:
+           total_loss = total_loss + amount
+
     print(f'Total Months: {total_months}')
     print(f'Profit: {total_profit}')
     print(f'Loss: {total_loss}')
-    
-          
-      
+   
     #The average of the changes in "Profit/Losses" over the entire period
     
     #The greatest increase in profits (date and amount) over the entire period
