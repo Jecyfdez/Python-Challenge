c32a08964f0391f394f91d9da9ab105e568b6ee3
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2020-09-07 01:00:07 -0700

    attempt second homework

---
+++ PyPoll/main.py
@@ -0,0 +1,30 @@
+import os
+import csv
+
+csvpath = os.path.join('..',)
+
+
+votes
+        candidate_list = list(set(candidate_list))
+        candidates["name"]= candidate_list
+       
+        
+
+#candidates = {"name":"Khan","Correy","Li","O'Tooley"}
+
+#The percentage of votes each candidate won
+
+#The total number of votes each candidate won
+
+#The winner of the election based on popular vote.
+
+print('Election Results')
+print('------------------------')
+print(f'Total Votes: {total_votes}')
+print('------------------------')
+print(candidates)
+print()
+print('------------------------')
+print('Winner:')
+print('------------------------')
+
