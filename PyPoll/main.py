aea6b7a958b668105c3c1a0919afa801aab25828
Jessica Fernandez <jessicafernandez@Jessicas-MBP.lan>
2020-09-07 19:33:57 -0700

    almost done with code, need winner and text file export

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,51 +1,88 @@
 import os
 import csv
 
+#create a path to open election_data.csv
 csvpath = os.path.join('..','PyPoll','Resources','election_data.csv')
-
-c ={}
-khan = 0
-correy = 0
-li = 0
-otooley = 0
+    
+    #Assign variables to outcomes wanted
+c = {}
+candidate_list = []
 total_votes = 0
+winner = 0
+
+khan_votes = 0
+khan_percent = 0
+
+otooley_votes = 0
+otooley_percent = 0
+
+correy_votes = 0
+correy_percent = 0
+
+li_votes = 0
+li_percent = 0
 
 with open(csvpath, newline='') as csvfile:
-#specify delimiter and variable "csvreader" that holds contents
+    #specify delimiter and variable "csvreader" that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-    #store a reference to a file stream
-    #print(csvreader)
+    
     #read the header row first
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
+    
     #candidate_list = list(set(candidate_list))
     #candidates["name"]= candidate_list
         
     for row in csvreader:
+        #The total number of votes cast
         total_votes = total_votes + 1
+        name = row[2]
+        candidate_list.append(name)
         
-        if row[2] == "Khan":
-            khan = khan +1
-        #if row[2] == Correy:
-            #correy= correy + 1
+    for name in candidate_list:
+        
+        #COMPLETE LIST OF CANDIDATES AND NUMBER OF VOTES USING DICT()
+        #c[name] = c.get(name,0) +1
+        
+        #candidate_list = list(set(candidate_list))
+        
+    #print(c)
+        #assign candidate votes to a variable to print in proper text format
+        #The total number of votes each candidate won
+        #The percentage of votes each candidate won
+        if name == "Khan":
+            khan_votes += 1
+            khan_percent = (khan_votes / total_votes)
+            khan_percent= "{:.3%}".format(khan_percent)
+             
+        if name == "O'Tooley":
+            otooley_votes += 1
+            otooley_percent = (otooley_votes / total_votes)
+            otooley_percent= "{:.3%}".format(otooley_percent)
+             
+        if name == "Correy":
+            correy_votes += 1
+            correy_percent = (correy_votes / correy_votes)
+            correy_percent= "{:.3%}".format(correy_percent)
             
-            
+        if name == "Li":
+            li_votes += 1
+            li_percent = (li_votes / total_votes)
+            li_percent= "{:.3%}".format(li_percent)
+    
+        #The winner of the election based on popular vote.
+        
 
-    #candidates = {"name":"Khan","Correy","Li","O'Tooley"}
-
-    #The percentage of votes each candidate won
-
-    #The total number of votes each candidate won
-
-    #The winner of the election based on popular vote.
 
     print('Election Results')
     print('------------------------')
     print(f'Total Votes: {total_votes}')
     print('------------------------')
-    print(f'Khan: ({khan})')
+   
     print()
+    print(f'{khan_votes} {otooley_votes} {correy_votes} {li_votes}')
     print('------------------------')
-    print('Winner:')
+    print('Winner: (winner)')
     print('------------------------')
+
+
 
